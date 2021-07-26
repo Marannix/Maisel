fda79f19244271777407f8dc63d4316a78da0f80
Josh <onabanjotobi@gmail.com>
2021-07-26 10:20:21 +0100

    Created sign in activity and added view binding

--- app/src/main/java/com/maisel/signin/SignInActivity.kt
+++ app/src/main/java/com/maisel/signin/SignInActivity.kt
@@ -2,10 +2,18 @@ package com.maisel.signin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.maisel.R
+import com.maisel.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
     }
 }
