d5487edfeaa5c9dcd41716d0a0f3b0ee927e3201
Joshua Onabanjo <onabanjotobi@gmail.com>
2021-10-01 14:21:03 +0100

    Added Email and Password text field

--- app/src/main/java/com/maisel/dashboard/composables/LoginPage1.kt
+++ app/src/main/java/com/maisel/dashboard/composables/LoginPage1.kt
@@ -4,30 +4,33 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.shape.ZeroCornerSize
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.maisel.R
 import com.maisel.ui.purplish
 
+//TODO: Support multi size
+//https://proandroiddev.com/supporting-different-screen-sizes-on-android-with-jetpack-compose-f215c13081bd
+// https://www.youtube.com/watch?v=iUIXsHiuRfY
 @Preview(showBackground = true)
 @Composable
 fun LoginPage1() {
-    val emailAddress = ""
-    val emailState = remember { mutableStateOf(TextFieldValue(emailAddress)) }
+
+    val emailState = remember { mutableStateOf(TextFieldValue("")) }
+    val passwordState = remember { mutableStateOf(TextFieldValue("")) }
 
     Box {
         //This is the background
@@ -38,8 +41,8 @@ fun LoginPage1() {
         //This is the white background section
         Surface(
             color = Color.White, modifier = Modifier
-                .height(600.dp)
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .fillMaxHeight(0.8f),
             shape = RoundedCornerShape(60.dp).copy(
                 topStart = ZeroCornerSize,
                 topEnd = ZeroCornerSize
@@ -54,22 +57,63 @@ fun LoginPage1() {
                     contentDescription = ""
                 )
                 Spacer(modifier = Modifier.padding(16.dp))
-                OutlinedTextField(
-                    value = emailState.value, onValueChange = {
-                        emailState.value = it
-                    },
-                    label = {
-                        Text(text = "Email address")
-                    },
-                    placeholder = {
-                        Text(text = "Email address")
-                    },
-                    leadingIcon = {
-                        Icon(imageVector = Icons.Filled.Email, contentDescription = "Email Icon")
-                    },
-                    modifier = modifier
-                )
+                CreateEmailAddressTextField(emailState, modifier)
+                Spacer(modifier = Modifier.padding(6.dp))
+                CreatePasswordTextField(passwordState, modifier)
+                Spacer(modifier = Modifier.padding(vertical = 12.dp))
+
+                //https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:compose/material/material/samples/src/main/java/androidx/compose/material/samples/ContentAlphaSamples.kt
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.disabled ) {
+                    Text("Forgot Password?", textAlign = TextAlign.End, modifier = modifier)
+                }
+
             }
         }
     }
+
+}
+
+@Composable
+private fun CreateEmailAddressTextField(
+    emailState: MutableState<TextFieldValue>,
+    modifier: Modifier
+) {
+    OutlinedTextField(
+        value = emailState.value, onValueChange = {
+            emailState.value = it
+        },
+        label = {
+            Text(text = "Email address")
+        },
+        placeholder = {
+            Text(text = "Email address")
+        },
+        leadingIcon = {
+            Icon(imageVector = Icons.Filled.Email, contentDescription = "Email Icon")
+        },
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun CreatePasswordTextField(
+    passwordState: MutableState<TextFieldValue>,
+    modifier: Modifier
+) {
+    OutlinedTextField(
+        value = passwordState.value, onValueChange = {
+            passwordState.value = it
+        },
+        label = {
+            Text(text = "Password")
+        },
+        placeholder = {
+            Text(text = "Password")
+        },
+        leadingIcon = {
+            Icon(imageVector = Icons.Filled.Lock, contentDescription = "Password Icon")
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        modifier = modifier
+    )
 }
